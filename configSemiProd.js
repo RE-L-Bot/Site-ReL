5503fce8661cc3b0527729953793dbe732383a62
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-07-21 23:15:08 -0300

    Change params in configuration file

--- configSemiProd.js
+++ configSemiProd.js
@@ -1,9 +1,11 @@
+const start = "siteredev4"
 const ends = ".squareweb.app"
-const url = `https://siteredev4${ends}`
-const CLIENT_ID = "1064938979406921738"
+const protocol = "https"
+const url = `${protocol}://${start}${ends}`
+const CLIENT_ID = "1018958083764002919"
 const REDIRECT_URI = `${url}/{lang}/dashboard`
 const API_ENDPOINT = "https://discord.com/api/v10"
-const linkRedirectDiscord = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=https%3A%2F%2Fsiteredev4.squareweb.app%2F{lang}%2Fdashboard&scope=identify+guilds+email`
-const linkRedirectAddBot = `https://discord.com/api/oauth2/authorize?response_type=token&client_id=${CLIENT_ID}&permissions=1635070044278&guild_id={guildID}&redirect_uri=https%3A%2F%siteredev4.squareweb.app%2F{lang}%2Fdashboard&response_type=code&scope=bot%20applications.commands%20identify%20guilds`
+const linkRedirectDiscord = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=https%3A%2F%2F${start}${ends}%2F{lang}%2Fdashboard&scope=identify+guilds+email`
+const linkRedirectAddBot = `https://discord.com/api/oauth2/authorize?response_type=token&client_id=${CLIENT_ID}&permissions=1635070044278&guild_id={guildID}&redirect_uri=https%3A%2F%${start}${ends}%2F{lang}%2Fdashboard&response_type=code&scope=bot%20applications.commands%20identify%20guilds`
 
 module.exports = { url, API_ENDPOINT, REDIRECT_URI, linkRedirectDiscord, linkRedirectAddBot }
