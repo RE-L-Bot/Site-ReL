202d1fa9cf82270f010f252df5616b6c9c333c86
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-07-21 23:16:17 -0300

    Nada de importante

--- pages/br/teste.jsx
+++ pages/br/teste.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import EmbedEditor from "@/components/br/embed/EmbedEditor";
 import EmbedBuilder from "@/components/br/embed/EmbedBuilder";
 import Header from "@/components/br/headers";
-import EmbedContructor from "@/components/br/embed/embedContructor";
-import FormEmbed from "@/components/br/formEmbed";
 
 export default function Teste() {
 
