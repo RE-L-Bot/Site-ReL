3d0a9bea9c6cf5781143fb5b6e3d69362045a398
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-08-28 17:21:11 -0300

    Preparando pagina de not found para receber outros idiomas

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,62 +1,24 @@
-import Head from "next/head"
-import HEADER from "@/components/br/headers"
-import Footer from "@/components/br/footers"
+import { usePathname } from "next/navigation"
+import Error404ComponentBR from "@/components/br/error404";
+import FooterBR from "@/components/br/footers";
+import HeaderBR from "@/components/br/headers";
 
 export default function Error404() {
 
-    return (
-        <div>
+    let lang = usePathname()
+    lang = lang.split("/")
+    lang = lang[1]
 
-            <Head>
-                <title>404 • RE=L</title>
-            </Head>
+    const dataSet = {
+        "br": <>
+            <HeaderBR />
+            <Error404ComponentBR />
+            <FooterBR />
+        </>
+    }
 
-            <HEADER />
+    if (lang !== "br") {
+        return dataSet[`br`]
+    }
 
-            <div className="divError">
-
-                <img className="ImageError" src="/gifs/error404.gif" />
-
-                <div>
-
-                    <h1>Erro 404 Pagina não encontrada</h1>
-
-                    <h3>Está pagina não foi encontrada nos arquivos do site</h3>
-
-                    <h4>Caso ache que é um erro reporte no servidor de suporte</h4>
-
-                    <div className="DivButtonSuportError">
-
-                        <button className="" onClick={redirectServidor}>
-
-                            <div>
-                                <div>
-                                    <img src="/svgs/discordiconSVG.svg" alt="discord icon" />
-                                </div>
-
-                                <div>
-                                    Meu servidor
-                                </div>
-                            </div>
-
-                        </button>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <Footer />
-
-        </div>
-    )
-
-}
-
-function redirectServidor() {
-    window.open(
-        "https://discord.gg/mmwUHd4Yzf",
-        "_blank"
-    )
 }
