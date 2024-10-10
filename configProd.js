c01d74e660fab164385726a074c72eb595f7afa7
Marciel404 <103785110+Marciel404@users.noreply.github.com>
2024-10-10 16:23:32 -0300

    Corrigindo link

--- configProd.js
+++ configProd.js
@@ -6,6 +6,6 @@ const CLIENT_ID = "1018958083764002919"
 const REDIRECT_URI = `${url}/{lang}/dashboard`
 const API_ENDPOINT = "https://discord.com/api/v10"
 const linkRedirectDiscord = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${protocol}%3A%2F%2F${start}${ends}%2F{lang}%2Fdashboard&scope=identify+guilds+email`
-const linkRedirectAddBot = `https://discord.com/oauth2/authorize?client_id=1064938979406921738&permissions=582183853288694&response_type=code&redirect_uri=${protocol}%3A%2F%2F${start}${ends}%2F{lang}%2Fdashboard&integration_type=0&scope=identify+guilds+bot+email+applications.commands`
+const linkRedirectAddBot = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&permissions=582183853288694&response_type=code&redirect_uri=${protocol}%3A%2F%2F${start}${ends}%2F{lang}%2Fdashboard&integration_type=0&scope=identify+guilds+bot+email+applications.commands`
 
 module.exports = { url, API_ENDPOINT, REDIRECT_URI, linkRedirectDiscord, linkRedirectAddBot }
